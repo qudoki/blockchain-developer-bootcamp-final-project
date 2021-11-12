1e7877e968dd411259c9aaaa858ee6c873f51f29
Qian Dong <qdong327@gmail.com>
2021-11-12 17:40:04 -0500

    game logic working

--- client/src/App.js
+++ client/src/App.js
@@ -13,81 +13,6 @@ require("dotenv").config();
 //Here is another example of using process.env
 // const APIKey = process.env.API_KEY;
 
-// // player movement logic
-var player = document.querySelector(".player-img");
-var map = document.querySelector(".map");
-// // state of player
-// var x = 0;
-// var y = 0;
-// var heldDirections = [];
-// var speed = 1;
-// const placePlayer = () => {
-// 	var pixelSize = parseInt(
-// 		getComputedStyle(document.documentElement).getPropertyValue(
-// 			"--pixel-size"
-// 		)
-// 	);
-// 	const heldDirection = heldDirections[0];
-// 	if (heldDirection) {
-// 		if (heldDirection === directions.right) {
-// 			x += speed;
-// 		}
-// 		if (heldDirection === directions.left) {
-// 			x -= speed;
-// 		}
-// 		if (heldDirection === directions.down) {
-// 			y += speed;
-// 		}
-// 		if (heldDirection === directions.up) {
-// 			y -= speed;
-// 		}
-// 		player.setAttribute("facing", heldDirection);
-// 	}
-// 	// player.setAttribute("walking", heldDirection ? "true" : "false");
-
-// 	var cameraLeft = pixelSize * 66;
-// 	var cameraTop = pixelSize * 42;
-
-// 	// player.style.transform = `translate3d( ${x*pixelSize}px, ${y*pixelSize}px, 0)`;
-// 	// map.style.transform = `translate3d( ${-x*pixelSize+cameraLeft}px, ${-y*pixelSize+cameraTop}px, 0)`;
-// };
-// // set up the game loop
-// const step = () => {
-// 	placePlayer();
-// 	window.requestAnimationFrame(() => {
-// 		step();
-// 	});
-// };
-// step();
-
-// //direction key state
-// const directions = {
-// 	up: "up",
-// 	down: "down",
-// 	left: "left",
-// 	right: "right",
-// };
-// const keys = {
-// 	38: directions.up,
-// 	37: directions.left,
-// 	39: directions.right,
-// 	40: directions.down,
-// };
-// document.addEventListener("keydown", (e) => {
-// 	var dir = keys[e.which];
-// 	if (dir && heldDirections.indexOf(dir) === -1) {
-// 		heldDirections.unshift(dir);
-// 	}
-// });
-// document.addEventListener("keyup", (e) => {
-// 	var dir = keys[e.which];
-// 	var index = heldDirections.indexOf(dir);
-// 	if (index > -1) {
-// 		heldDirections.splice(index, 1);
-// 	}
-// });
-// // end of player movement logic
-
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -125,6 +50,107 @@ class App extends Component {
 				{ web3, accounts, balance, contract: instance },
 				this.runExample
 			);
+
+			// character movement logic
+			var character = document.querySelector(".Character");
+			var map = document.querySelector(".map");
+
+			// state of character
+			var x = 40;
+			var y = 35;
+			var heldDirections = [];
+			var speed = 0.75;
+
+			const placeCharacter = () => {
+				var pixelSize = parseInt(
+					getComputedStyle(document.documentElement).getPropertyValue(
+						"--pixel-size"
+					)
+				);
+				const heldDirection = heldDirections[0];
+				if (heldDirection) {
+					if (heldDirection === directions.right) {
+						x += speed;
+					}
+					if (heldDirection === directions.left) {
+						x -= speed;
+					}
+					if (heldDirection === directions.down) {
+						y += speed;
+					}
+					if (heldDirection === directions.up) {
+						y -= speed;
+					}
+					character.setAttribute("facing", heldDirection);
+				}
+				character.setAttribute("walking", heldDirection ? "true" : "false");
+
+				// wall boundaries
+				var leftLimit = 35;
+				var rightLimit = 16 * 11 + 8;
+				var topLimit = -8 + 32;
+				var bottomLimit = 16 * 7 + 25;
+				if (x < leftLimit) {
+					x = leftLimit;
+				}
+				if (x > rightLimit) {
+					x = rightLimit;
+				}
+				if (y < topLimit) {
+					y = topLimit;
+				}
+				if (y > bottomLimit) {
+					y = bottomLimit;
+				}
+
+				var camera_left = pixelSize * 66;
+				var camera_top = pixelSize * 42;
+
+				map.style.transform = `translate3d( ${
+					-x * pixelSize + camera_left
+				}px, ${-y * pixelSize + camera_top}px, 0 )`;
+				character.style.transform = `translate3d( ${x * pixelSize}px, ${
+					y * pixelSize
+				}px, 0 )`;
+			};
+
+			// set up the game loop
+			const step = () => {
+				placeCharacter();
+				window.requestAnimationFrame(() => {
+					step();
+				});
+			};
+			step();
+
+			//direction key state
+			const directions = {
+				up: "up",
+				down: "down",
+				left: "left",
+				right: "right",
+			};
+			const keys = {
+				38: directions.up,
+				37: directions.left,
+				39: directions.right,
+				40: directions.down,
+			};
+			document.addEventListener("keydown", (e) => {
+				var dir = keys[e.which];
+				if (dir && heldDirections.indexOf(dir) === -1) {
+					heldDirections.unshift(dir);
+				}
+			});
+			document.addEventListener("keyup", (e) => {
+				var dir = keys[e.which];
+				var index = heldDirections.indexOf(dir);
+				if (index > -1) {
+					heldDirections.splice(index, 1);
+				}
+			});
+			// end of player movement logic
+
 		} catch (error) {
 			// Catch any errors for any of the above operations.
 			alert(
@@ -154,30 +180,30 @@ class App extends Component {
 		return (
 			<div className="App">
 				<h1>NFT Gallery</h1>
-				<h2>Smart Contract Example</h2>
-				<p>
+				{/* <h2>Smart Contract Example</h2> */}
+				{/* <p>
 					If your contracts compiled and migrated successfully, below will show
 					a stored value of 20 (by default). Try changing the value stored on{" "}
 					<strong>line 95</strong> of App.js.
-				</p>
+				</p> */}
 				<div>The stored value is: {this.state.storageValue}</div>
 
 				<div>The account address is: {this.state.accounts}</div>
 				<div>The account balance is: Ξ {this.state.balance}</div>
-				<div class="Character">
-					<img class="Character_spritesheet pixelart face-up" src="../../assets/Sprite-BG.png" />
-				</div>
-				{/* <div
-					class="player-img"
-					alt="player"
-					src="../../assets/Player.png"
-				></div> */}
-				<div class="camera">
+				<div className="camera">
 					<div
-						class="map background-img"
-						alt=""
+						className="map background-img"
+						alt="mapping"
 						src="../../assets/Gallery-Base.png"
-					></div>
+					>
+						<div className="Character">
+							<img
+								className="Character_spritesheet pixelart face-up"
+								alt="characer"
+								src="../../assets/Sprite-BG.png"
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
