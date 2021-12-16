50f03a4a6a38895dc33ca079f29e00a72be86317
Qian Dong <qdong327@gmail.com>
2021-12-16 18:26:22 -0500

    transaction receipt test and purchase test failing

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -11,41 +11,6 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract NFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenCounter;
-    mapping(uint256 => string) _tokenURIs;
-
-    // has owner
-    address _owner = msg.sender;
-
-    // creates role for minting only
-    mapping(address => bool) public galleryOnlyMinter;
-
-    function getMinterRole(address _account) public view returns (bool) {
-        return galleryOnlyMinter[_account];
-    }
-
-    function addMinter(address _minter) public payable {
-        // require(_minter != address(0));
-        require(galleryOnlyMinter[_minter] == false);
-        galleryOnlyMinter[_minter] = true;
-    }
-
-    // contract can get data and msg not destroyed
-    receive() external payable {}
-    fallback() external payable {}
-
-    // initialize contract while deployment with contract's collection name and token
-    constructor(address payable gallery) ERC721("NFT Gallery", "NFT") {
-        // _setBaseURI("ipfs://");
-        // _owner = gallery;
-        // tokenCounter = 0;
-    }
-
-    // <enum State: ForSale, NotForSale, Pending>
-    enum State {
-        ForSale,
-        NotForSale,
-        Pending
-    }
     struct Item {
         uint256 tokenId;
         string title;
@@ -55,61 +20,159 @@ contract NFT is ERC721URIStorage, Ownable {
         address payable currentOwner;
         address payable buyer;
         uint256 price;
-        uint256 numberOfTransfers;
-        State status;
+        bool forSale;
     }
 
-    mapping(uint256 => Item) public collection;
+    // Puts all pieces in a collection
+    Item[] public collection;
 
-    //Checks total supply
+    // Has owner
+    address payable public _owner;
+
+    // FOR MINTING & MINTER ROLE
+    // Creates role for minting only
+    mapping(address => bool) public galleryOnlyMinter;
+
+    function getMinterRole(address _account) public view returns (bool) {
+        return galleryOnlyMinter[_account];
+    }
+
+    function addMinter(address _minter) public payable {
+        require(galleryOnlyMinter[_minter] == false);
+        galleryOnlyMinter[_minter] = true;
+    }
+
+    // Contract can get data and msg not destroyed
+    receive() external payable {}
+
+    fallback() external payable {}
+
+    // FOR CHECKING TOTAL SUPPLY
     function totalSupply() public view returns (uint256) {
         return _tokenCounter.current();
     }
 
-    function _setTokenURI(uint256 tokenId, string memory _tokenURI) override internal {
+    // Initialize contract while deployment with contract's collection name and token
+    constructor(address payable gallery) ERC721("NFT Gallery", "NFT") {
+        // _owner = msg.sender;
+        // _setBaseURI("ipfs://");
+        // tokenCounter = 0;
+    }
+
+    //Setting NFT info to collection
+    function _setNft(uint256 _tokenId, string memory _title, string memory _artist, string memory _tokenURI, address payable _gallery, address payable _currentOwner, address payable _buyer, uint256 _price) public {
+        Item memory item;
+        item.tokenId = _tokenId;
+        item.artist = _artist;
+        item.tokenURI = _tokenURI;
+        item.gallery = _gallery;
+        item.currentOwner = _currentOwner;
+        item.buyer = _buyer;
+        item.price = _price;
+        item.title = _title;
+        item.forSale = true;
+        collection.push(item);
+        // mint(_gallery, _price, _tokenURI);
+        // return Item;
+    }
+
+    //Getting the collection data
+    function getNft(uint _index) public view returns (uint256 _tokenId, bool _forSale) {
+        Item storage item = collection[_index];
+        return (item.tokenId, item.forSale);
+    }
+
+    event Mint(address owner, uint256 price, uint256 id, string uri);
+
+    //Mint all - how to get preloaded IPFS files upon connection -- only one right now
+    function mint(uint256 _tokenId, string memory _title, string memory _artist, string memory _tokenURI, address payable _gallery, address payable _currentOwner, address payable _buyer, uint256 _price) public returns (uint256) {
+        require(galleryOnlyMinter[_gallery], "Only the gallery can mint!");
+        uint256 tokenId = _tokenCounter.current();
+        _tokenCounter.increment();
+        _safeMint(_gallery, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        _setNft(_tokenId, _title, _artist, _tokenURI, _gallery, _currentOwner, _buyer, _price);
+        emit Mint(_gallery, _price, tokenId, _tokenURI);
+        return tokenId;
+    }
+
+
+
+
+
+
+
+
+    modifier isOwner() {
+        require(msg.sender == _owner, "Not the owner!");
+        _;
+    }
+
+    mapping(uint256 => string) _tokenURIs;
+    mapping(uint256 => bool) public forSale;
+    mapping(uint256 => uint256) public price;
+    // Mapping from token ID to owner address
+    mapping(uint256 => address) private _owners;
+    // Mapping owner address to token count
+    mapping(address => uint256) private _balances;
+
+
+
+    // Figure out how to return token Id
+    // function getTokenId(uint256 tokenId, address owner) public view returns (uint, uint) {
+
+    // }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        override
+    {
         _tokenURIs[tokenId] = _tokenURI;
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
         require(_exists(tokenId));
         string memory _tokenURI = _tokenURIs[tokenId];
         return _tokenURI;
     }
 
-    //Mint all preloaded IPFS files upon connection -- only one right now
-    function mint(address to, string memory uri) public returns (uint256) {
-        require(galleryOnlyMinter[to], "Only the gallery can mint!");
-        uint256 tokenId = _tokenCounter.current();
-        _tokenCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-        return tokenId;
-    }
 
-    // modifier isOwner() {
-    //     require(msg.sender == _owner, "Not the owner!");
-    //     _;
+
+    //From Open Zepp for reference
+    // function _transfer(address from, address to, uint256 tokenId) internal override virtual {
+    //     require(ERC721.ownerOf(tokenId) == from, "ERC721: transfer from incorrect owner");
+    //     require(to != address(0), "ERC721: transfer to the zero address not allowed");
+    //     _beforeTokenTransfer(from, to, tokenId);
+    //     //Clear approvals from the previous owner
+    //     _approve(address(0), tokenId);
+    //     _balances[from] -= 1;
+    //     _balances[to] += 1;
+    //     _owners[tokenId] = to;
+    //     emit Transfer(from, to, tokenId);
     // }
 
     event Purchase(address owner, uint256 price, uint256 id, string uri);
 
-        // function buy(uint256 _id) public payable {
-        //     _validate(_id);
-        //     _trade(_id);
-        //     emit Purchase(msg.sender, price[_id], _id, tokenURI(_id));
-        // }
+    function buy(uint256 _id) external payable {
+        _validate(_id);
+        _trade(_id);
+        emit Purchase(msg.sender, price[_id], _id, tokenURI(_id));
+    }
 
-        // //If for sale and balance is enough
-        // function _validate(uint256 _id) internal {
-        //     require(_exists(_id), "Error, wrong Token id");
-        //     require(!sold[_id], "Error, Token is not for sale");
-        //     require(msg.value >= price[_id], "Error, Token costs more");
-        // }
+    function _validate(uint256 _id) internal {
+        require(_exists(_id), "Error, wrong Token id");
+        require(msg.value >= price[_id], "Error, Token costs more");
+    }
 
-        // function _trade(uint256 _id) internal {
-        //     safeTransferFrom(address(this), msg.sender, _id);
-        //     sold[_id] = true;
-        // }
-
-    
+    function _trade(uint256 _id) internal {
+        _transfer(address(this), msg.sender, _id);
+        _owner.transfer(msg.value);
+        forSale[_id] = true;
+    }
 }
--- test/nft.test.js
+++ test/nft.test.js
@@ -8,17 +8,18 @@ const {
 	isPayable,
 	isType,
 } = require("./ast-helper");
+const { providers } = require("ethers");
 
 contract("NFT", function (accounts) {
 	const [_owner, alice, bob] = accounts;
 	// currently gallery is address at 0
-	const gallery = "0x9AB2179A85e6F7c2AdF3584f8167a84C84D528ef";
+	const gallery = "0x33081c10C4732C902d7A82A4e4b56E71Da0Ef53c";
 	const price = 1;
-	const name = "nft";
 	const artist = "john doe";
 	const title = "best nft ever";
 	const buyer = "buyerperson";
 	const currentOwner = "ownerperson";
+	const tokenId = 1;
 	const uri =
 		"https://ipfs.io/ipfs/bafybeido4wnjbmthgpygr5wubsiodnavmdbmlf7hbp262leaptffls2qdm";
 	let instance;
@@ -33,35 +34,6 @@ contract("NFT", function (accounts) {
 				typeof instance.owner,
 				"function",
 				"the contract has no owner"
-			);
-		});
-	});
-
-	describe("Enum State", () => {
-		let enumState;
-		before(() => {
-			enumState = NFT.enums.State;
-			assert(enumState, "The contract should define an Enum called State");
-		});
-
-		it("should define `ForSale`", () => {
-			assert(
-				enumState.hasOwnProperty("ForSale"),
-				"The enum does not have a `ForSale` value"
-			);
-		});
-
-		it("should define `NotForSale`", () => {
-			assert(
-				enumState.hasOwnProperty("NotForSale"),
-				"The enum does not have a `NotForSale` value"
-			);
-		});
-
-		it("should define `Pending`", () => {
-			assert(
-				enumState.hasOwnProperty("Pending"),
-				"The enum does not have a `Pending` value"
 			);
 		});
 	});
@@ -154,76 +126,145 @@ contract("NFT", function (accounts) {
 		});
 
 		it("piece should be minted with provided uri", async () => {
-			// const mintStatus = await instance.mint(gallery, uri);
-			// console.log(mintStatus);
-			const uri = "https://ipfs.io/ipfs/bafybeido4wnjbmthgpygr5wubsiodnavmdbmlf7hbp262leaptffls2qdm";
 			await instance.addMinter(accounts[0]);
-			await instance.mint(accounts[0], uri);
+			await instance.mint(
+				tokenId,
+				title,
+				artist,
+				uri,
+				accounts[0],
+				accounts[0],
+				accounts[2],
+				price
+			);
 			const tokenUri = await instance.tokenURI(0);
 			const balanceOfOwner = await instance.balanceOf(accounts[0]);
 			assert.equal(tokenUri, uri);
 			assert.equal(balanceOfOwner, 1);
-			// console.log(tokenUri);
-		})
+		});
 
-		// // not done - purchase and transfer
+		// error when not enough
+		it("should error when not enough value is sent when purchasing an item", async () => {
+			await instance.addMinter(accounts[0]);
+			await instance.mint(
+				tokenId,
+				title,
+				artist,
+				uri,
+				accounts[0],
+				accounts[0],
+				accounts[2],
+				price
+			);
+			await catchRevert(instance.buy(0, { from: bob, value: 1 }));
+		});
+
+		// not done - push to collection? return id not working
+
+		it("piece should return tokenId from transaction receipt", async () => {
+			await instance.addMinter(accounts[0]);
+			await instance.mint(
+				tokenId,
+				title,
+				artist,
+				uri,
+				accounts[0],
+				accounts[0],
+				accounts[2],
+				price
+			);
+
+			await web3.eth.getTransactionReceipt(accounts[0]).then(function(data){
+				console.log(data);
+				// let returnedId;
+				// let logs = data.logs;
+				// returnedId = console.log(web3.utils.hexToNumber(logs[0].topics[3]));
+				// console.log(returnedId);
+			})
+
+			assert.equal(returnedId, tokenId);
+
+			// await instance.setNft(
+			// 	title,
+			// 	artist,
+			// 	uri,
+			// 	gallery,
+			// 	currentOwner,
+			// 	buyer,
+			// 	price
+			// );
+		});
+
+
+
+
+		// not done - purchase and transfer
+
 		// it("should allow someone to purchase an item and update state accordingly", async () => {
-		// 	await instance.addItem(name, price, { from: alice });
-		// 	var aliceBalanceBefore = await web3.eth.getBalance(alice);
+		// 	await instance.addMinter(accounts[0]);
+		// 	await instance.mint(
+		// 		tokenId,
+		// 		title,
+		// 		artist,
+		// 		uri,
+		// 		accounts[0],
+		// 		accounts[0],
+		// 		accounts[2],
+		// 		price
+		// 	);
+		// 	const index = await instance.totalSupply();
+		// 	console.log(index);
+		// 	// // await instance.buy(name, price, { from: accounts[0] });
+		// 	var galleryBalanceBefore = await web3.eth.getBalance(accounts[0]);
 		// 	var bobBalanceBefore = await web3.eth.getBalance(bob);
-
-		// 	await instance.buyItem(0, { from: bob, value: excessAmount });
-
-		// 	var aliceBalanceAfter = await web3.eth.getBalance(alice);
+		// 	console.log(galleryBalanceBefore);
+		// 	console.log(bobBalanceBefore);
+		// 	await instance.buy(tokenId.tokenId); // this should be tokenId - how to get?
+		// 	var galleryBalanceAfter = await web3.eth.getBalance(accounts[0]);
 		// 	var bobBalanceAfter = await web3.eth.getBalance(bob);
-
+		// 	console.log(galleryBalanceAfter);
+		// 	console.log(bobBalanceAfter);
 		// 	const result = await instance.fetchItem.call(0);
-
 		// 	assert.equal(
 		// 		result[3].toString(10),
 		// 		NFT.State.Sold,
 		// 		'the state of the item should be "Sold"'
 		// 	);
-
 		// 	assert.equal(
 		// 		result[5],
 		// 		bob,
 		// 		"the buyer address should be set bob when he purchases an item"
 		// 	);
-
-		// 	assert.equal(
-		// 		new BN(aliceBalanceAfter).toString(),
-		// 		new BN(aliceBalanceBefore).add(new BN(price)).toString(),
-		// 		"alice's balance should be increased by the price of the item"
-		// 	);
-
-		// 	assert.isBelow(
-		// 		Number(bobBalanceAfter),
-		// 		Number(new BN(bobBalanceBefore).sub(new BN(price))),
-		// 		"bob's balance should be reduced by more than the price of the item (including gas costs)"
-		// 	);
+		// 	// assert.equal(
+		// 	// 	new BN(galleryBalanceAfter).toString(),
+		// 	// 	new BN(galleryBalanceBefore).add(new BN(price)).toString(),
+		// 	// 	"gallery's balance should be increased by the price of the item"
+		// 	// );
+		// 	// assert.isBelow(
+		// 	// 	Number(bobBalanceAfter),
+		// 	// 	Number(new BN(bobBalanceBefore).sub(new BN(price))),
+		// 	// 	"bob's balance should be reduced by more than the price of the item (including gas costs)"
+		// 	// );
+		// 	// });
+		// 	// not done - should emit sold event
+		// 	// it("should emit LogSold event when and item is purchased", async () => {
+		// 	// 	var eventEmitted = false;
+		// 	// 	await instance.addMinter(accounts[0]);
+		// 	// 	await instance.mint(accounts[0], uri);
+		// 	// 	const tx = await instance.buy(0, { from: bob, value: excessAmount });
+		// 	// 	if (tx.logs[0].event == "LogSold") {
+		// 	// 		eventEmitted = true;
+		// 	// 	}
+		// 	// 	assert.equal(
+		// 	// 		eventEmitted,
+		// 	// 		true,
+		// 	// 		"adding an item should emit a Sold event"
+		// 	// 	);
 		// });
-		// // not done - error when not enough
-		// it("should error when not enough value is sent when purchasing an item", async () => {
-		// 	await instance.addItem(name, price, { from: alice });
-		// 	await catchRevert(instance.buyItem(0, { from: bob, value: 1 }));
-		// });
-		// // not done - should emit sold event
-		// it("should emit LogSold event when and item is purchased", async () => {
-		// 	var eventEmitted = false;
 
-		// 	await instance.addItem(name, price, { from: alice });
-		// 	const tx = await instance.buyItem(0, { from: bob, value: excessAmount });
 
-		// 	if (tx.logs[0].event == "LogSold") {
-		// 		eventEmitted = true;
-		// 	}
 
-		// 	assert.equal(
-		// 		eventEmitted,
-		// 		true,
-		// 		"adding an item should emit a Sold event"
-		// 	);
-		// });
+
+
 	});
 });
