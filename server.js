a514a50443714a2abea7023bc6cda66a71019ea1
Qian Dong <qdong327@gmail.com>
2022-01-12 17:33:25 -0500

    changed sprite

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 app.get("/", function (req, res) {
 	console.log(path.join(__dirname, "build"));
-	res.sendFile(path.join(__dirname, "/build/index.html"));
+	res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // Start the API server
