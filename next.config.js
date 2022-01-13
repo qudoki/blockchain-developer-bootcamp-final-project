166d608ce2a6924275d71c4ebddbdab0042b526c
Qian Dong <qdong327@gmail.com>
2022-01-13 11:08:56 -0500

    reverting to try commit yesterday

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 module.exports = {
 	webpack5: true,
 	webpack: (config) => {
-		config.resolve.fallback = {
-            ...config.resolve.fallback,
-            fs: false, os: false };
+		config.resolve.fallback = { fs: false };
 
 		return config;
 	},
